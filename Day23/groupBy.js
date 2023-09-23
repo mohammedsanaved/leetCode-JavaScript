a30b121965eac52147731d7a51c597f52edeb39c
Mohammed Sanaved <mosanaved@gmail.com>
2023-09-23 19:35:03 +0530

    group the array element

---
+++ Day23/groupBy.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @return {Array}
+ */
+Array.prototype.groupBy = function (fn) {
+  const grouped = {};
+
+  this.forEach((item) => {
+    const key = fn(item);
+    if (!grouped[key]) {
+      grouped[key] = [];
+    }
+    grouped[key].push(item);
+  });
+
+  return grouped;
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
