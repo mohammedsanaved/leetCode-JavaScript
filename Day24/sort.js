ae812bdb03c608f6575303d6935d12dc10c789dc
Mohammed Sanaved <mosanaved@gmail.com>
2023-09-23 19:42:03 +0530

    sort the array or object

---
+++ Day24/sort.js
@@ -0,0 +1,11 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function (arr, fn) {
+  arr.sort((a, b) => fn(a) - fn(b));
+  return arr;
+
+  return sortedArr;
+};
